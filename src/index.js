8fbe1243b7043e19d9dd0419085b1757f28369f1
Fodlu_dev <66522961+fodlu@users.noreply.github.com>
2024-12-22 14:15:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './components/App';
+import App from './components/App';
 // import DateCounter from './components/DateCounter';
-import BankApp from './components/BankApp';
+// import BankApp from './components/BankApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
+    <App />
     {/* <DateCounter /> */}
-    <BankApp />
+{/* <BankApp /> */}
   </React.StrictMode>
 );
